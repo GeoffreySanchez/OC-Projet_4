99ff6c7320e4f377d8f3011dba5e6d9d1f0f615e
geoffrey.sanchez-pro@outlook.fr <geoffrey.sanchez-pro@outlook.fr>
2018-06-07 17:23:09 +0200

    Modification design footer

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -26,13 +26,17 @@
         <footer>
             <div id="contact">
                 <h4>Informations de contact: </h4>
-                <div id="contactMail">
-                    <img src="../../public/images/mail.png" alt="icone de mail" />
-                    <p>: Jean.forteroche@contact.fr</p>
-                </div>
-                <div id="contactCourrier">
-                    <img src="../../public/images/adresse.png" alt="icone de courrier" />
-                    <a href="https://www.google.com.br/maps/place/109+Rue+du+Bac,+75007+Paris/@48.8523109,2.323076,18z/data=!3m1!4b1!4m5!3m4!1s0x47e671d48f41b323:0xd28da53e242a5cba!8m2!3d48.8523099!4d2.3237267">: <span class="underline">Auto-édition Forteroche - 109 Rue du bac - 2e étage - 75007 Paris</span></a>
+                <div id="infosContact">
+                    <div id="contactMail">
+                        <img src="../../public/images/mail.png" alt="icone de mail" />
+                        <p>: Jean.forteroche@contact.fr</p>
+                    </div>
+                    <div id="contactCourrier">
+                        <img src="../../public/images/adresse.png" alt="icone de courrier" />
+                        <p>
+                        <a href="https://www.google.com.br/maps/place/109+Rue+du+Bac,+75007+Paris/@48.8523109,2.323076,18z/data=!3m1!4b1!4m5!3m4!1s0x47e671d48f41b323:0xd28da53e242a5cba!8m2!3d48.8523099!4d2.3237267">: <span class="underline">Auto-édition Forteroche - 109 Rue du bac - 2e étage - 75007 Paris</span></a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </footer>
